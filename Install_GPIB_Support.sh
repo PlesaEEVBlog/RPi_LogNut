07f6b2fa01f5fb2d91ba02c96776077c02bafa3a
PlesaEEVBlog <caltrop.charles@gmail.com>
2017-03-08 22:20:12 +0100

    Update Install_GPIB_Support.sh

--- Install_GPIB_Support.sh
+++ Install_GPIB_Support.sh
@@ -39,7 +39,7 @@ cd opt
 sudo wget https://downloads.sourceforge.net/project/linux-gpib/linux-gpib%20for%203.x.x%20and%202.6.x%20kernels/4.0.4/linux-gpib-4.0.4rc2.tar.gz
 sudo tar xvzf linux-gpib-4.0.4rc2.tar.gz
 #sudo svn checkout http://svn.code.sf.net/p/linux-gpib/code/trunk/linux-gpib/ linux-gpib
-cd cd linux-gpib-4.0.4rc2
+cd linux-gpib-4.0.4rc2
 sudo ./bootstrap
 sudo ./configure
 sudo make clean
